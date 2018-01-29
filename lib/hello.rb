9a89be4f4664c019654ccae443396ad44b019480
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 18:03:29 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 def hello_t(array)
   if block_given?
      i = 0
-    
+
        while i < array.length
          yield array[i]
          i = i + 1
@@ -13,4 +13,3 @@ def hello_t(array)
 end
 
 # call your method here!
-
